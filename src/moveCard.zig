f3afa697e86928e26862120f34970603861217a6
d0mb1 <basti.rance@gmail.com>
2025-01-20 23:35:42 +0100

    added basic logic for moving cards in the bottom row

--- src/moveCard.zig
+++ src/moveCard.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const main = @import("main.zig");
+const helpFn = @import("helpFn.zig");
 
 // prototyping a funciton that will move cards between decks
 pub fn moveCardTestPrint() void {
@@ -43,14 +44,22 @@ pub fn flipCard() void {
 // moves cards between stack in to bottom field
 pub fn bottom2bottomMove(row_from: u8, column_from: u8, column_to: u8) void {
     var amout_of_cards: usize = 0;
-    var index_to: usize = 0;
+    var row_to: usize = 0;
     // finds the first spot where isn't a card in the column where we want to put our chosen card
-    while (main.bottom_field[index_to][column_to].value != @intFromEnum(main.Value.joker)) : (index_to += 1) {}
+    while (main.bottom_field[row_to][column_to].value != @intFromEnum(main.Value.joker)) : (row_to += 1) {}
     // finds out how mave cards should move
     while (main.bottom_field[row_from + amout_of_cards][column_from].value != @intFromEnum(main.Value.joker)) : (amout_of_cards += 1) {}
     var index: usize = 0;
-    while (index < amout_of_cards) : (index += 1) {
-        main.bottom_field[index_to + index][column_to] = main.bottom_field[row_from + index][column_from];
-        main.bottom_field[row_from + index][column_from].value = @intFromEnum(main.Value.joker);
+    // if the card is a different color and its value is one less then the one we're placing it on place the card. Else do nothing
+    if (main.bottom_field[row_from][column_from].value == main.bottom_field[row_to - 1][column_to].value - 1 and helpFn.isRed(main.bottom_field[row_from][column_from].shape) != helpFn.isRed(main.bottom_field[row_to - 1][column_to].shape)) {
+        // if the card is the only card in the stack, there's no reason to uncover the card below it cuz there's none
+        if (row_from != 0) {
+            main.bottom_field[row_from - 1][column_from].visivility = @intFromEnum(main.Visibility.uncovered);
+        }
+        // loops over all the cards that should be moved and moves them
+        while (index < amout_of_cards) : (index += 1) {
+            main.bottom_field[row_to + index][column_to] = main.bottom_field[row_from + index][column_from];
+            main.bottom_field[row_from + index][column_from].value = @intFromEnum(main.Value.joker);
+        }
     }
 }
